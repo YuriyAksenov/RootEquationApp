883334dd42b11870d885b34bbd7f4e64123fc4c3
Yuriy Aksenov <Yura290610@yandex.ru>
2017-07-02 23:57:15 +0300

    Добавьте файлы проекта.

---
+++ QuadraticEquation/Equation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootEquation
+{
+    public static class Equation
+    {
+        public delegate double Function(double x);
+
+        public static double HalfDivisionMethod(Function func, double startPosition, double endPosition, double accuracy  = 0.00001)
+        {
+            double middle = startPosition/2 + endPosition / 2;
+            double valueStart = func(startPosition);
+            double valueMiddle = func(middle);
+
+            double d = valueStart * valueMiddle;
+
+            if (d > 0)
+            {
+                startPosition = middle;
+            }
+            else endPosition = middle;
+
+            double difference = Math.Abs(startPosition - endPosition);
+
+            if (difference >= accuracy) return HalfDivisionMethod(func, startPosition, endPosition, accuracy);
+            
+            return startPosition/2+ endPosition / 2;
+        }
+
+
+    }
+}
---
+++ RootEquationApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RootEquation;
+using System.Threading;
+
+namespace RootEquationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action1 = () =>
+           {
+               var result = Equation.HalfDivisionMethod((x) =>
+               {
+                   Thread.Sleep(1000);
+                   return (x * x);
+               }, -0.5, 0.5);
+
+               Console.WriteLine("1 action =" + Math.Round(result, 5));
+           };
+
+            Action action2 = () =>
+            {
+                var result = Equation.HalfDivisionMethod((x) =>
+                {
+                    Thread.Sleep(10);
+                    return (x * x);
+                }, -0.5, 0.5);
+
+                Console.WriteLine("2 action =" + Math.Round(result, 5));
+            };
+
+            Action action3 = () =>
+            {
+                var result = Equation.HalfDivisionMethod((x) =>
+                {
+                    Thread.Sleep(500);
+                    return (x * x);
+                    //return 4 * (Math.Pow(x, 2) + 1) * Math.Log(x) - 1;
+                }, -0.5, 0.5);
+
+                Console.WriteLine("3 action =" + Math.Round(result, 5));
+            };
+
+            var asyncResult1 = action1.BeginInvoke(null,null);
+
+            var asyncResult2 = action2.BeginInvoke(null, null);
+
+            var asyncResult3 = action3.BeginInvoke(null, null);
+
+            action1.EndInvoke(asyncResult1);
+
+            action2.EndInvoke(asyncResult2);
+
+            action3.EndInvoke(asyncResult3);
+        }
+    }
+}
